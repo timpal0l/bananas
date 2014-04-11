6bdb377e872314818bc749d1189dc22ec42bbb95
Tim Isbister <timisbister@gmail.com>
2014-04-11 02:44:42 +0200

    clean

--- src/js/main.js
+++ src/js/main.js
@@ -1,24 +1,32 @@
-var HEIGHT = 768;
-var WIDTH = 1024;
-var canvas;
-var context;
+HEIGHT = 700;
+WIDTH = 1300;
 
 window.onload = function(){
-	generatePage();
+	setCanvas();
+	window.addEventListener("mousemove", foo, false);
 }
 
-function generatePage(){
+function setCanvas(){
 
 	canvas = document.getElementById('myCanvas');
 	context = canvas.getContext('2d');
-
-	canvas.width = WIDTH;
-	canvas.height = HEIGHT;
-
-	context.fillStyle = "black";
-	context.font = "bold 50px Consolas";
-	context.fillText("Hej hej :)", 400, 350);
 	
+	canvas.height = HEIGHT;
+	canvas.width = WIDTH;
+
 	var bottomMenu = new Menu();
 	drawMenu(bottomMenu);	
+
+}
+
+function foo(e){
+	//context.clearRect(700,200,700,200);
+	//funny function that follows the mouse!
+	
+	var xPos = e.clientX;
+	var yPos = e.clientY;
+	
+	context.fillStyle = "lightgreen";
+	context.font = "bold 20px Consolas";
+	context.fillText(".", xPos - 192, yPos);
 }
