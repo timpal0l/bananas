ecaedb6a737e7e734c294d081b0dbf1b9eba745d
Jonatan Moritz <jonatan.moritz@gmail.com>
2014-04-14 17:55:43 +0200

    Framerequest

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,21 @@
 HEIGHT = 700;
 WIDTH = 1300;
 
+//Request frame from browser
+window.requestAnimFrame = (function(){
+    return  window.requestAnimationFrame       ||
+            window.webkitRequestAnimationFrame ||
+            window.mozRequestAnimationFrame    ||
+            window.oRequestAnimationFrame      ||
+            window.msRequestAnimationFrame     ||
+            function( callback ){
+              window.setTimeout(callback, 1000 / 60);
+            };
+})();
+
 window.onload = function(){
 	setCanvas();
-	window.addEventListener("mousemove", foo, false);
+	window.addEventListener("mousemove", foo, false);	
 }
 
 function setCanvas(){
@@ -19,6 +31,7 @@ function setCanvas(){
 
 }
 
+	
 function foo(e){
 	//context.clearRect(700,200,700,200);
 	//funny function that follows the mouse!
