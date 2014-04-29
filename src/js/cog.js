d2158a8a8c4bd1057dc4d2ab95d7634a4bcac0dc
Conny Andersson <connyandersson88@gmail.com>
2014-04-29 10:16:47 +0200

    updated cog class and added model, where data objects will be stored, accessed, and modified

--- src/js/cog.js
+++ src/js/cog.js
@@ -1,11 +1,15 @@
 /* A cog class */
 
-function cog(shape, size, direction, speed) {
+function Cog(shape, x, y, height, width, direction, speed, color) {
 
 	this.shape = shape;
-	this.size = size;
+	this.x = x;
+	this.y = y;
+	this.height = height;
+	this.width = width;
 	this.direction = direction;
 	this.speed = speed;
+	this.color = color; // for example '#444444'
 }
 
 /*
@@ -24,28 +28,59 @@ function cog(shape, size, direction, speed) {
  * 
  */
 
+// Setters
 cog.prototype.setShape = function(arg) {
 	this.shape = arg;
 };
 
-cog.prototype.setSize = function(arg) {
-	this.shape = arg;
+cog.prototype.setX = function(arg) {
+	this.x = arg;
+};
+
+cog.prototype.setY = function(arg) {
+	this.y = arg;
+};
+
+cog.prototype.setHeight = function(arg) {
+	this.height = arg;
+};
+
+cog.prototype.setWidth = function(arg) {
+	this.width = arg;
 };
 
 cog.prototype.setDirection = function(arg) {
-	this.shape = arg;
+	this.direction = arg;
 };
 
 cog.prototype.setSpeed = function(arg) {
-	this.shape = arg;
+	this.speed = arg;
 };
+
+cog.prototype.setColor = function(arg) {
+	this.color = arg;
+};
+
+// Getters
 
 cog.prototype.getShape = function() {
 	return this.shape;
 };
 
-cog.prototype.getSize = function() {
-	return this.size;
+cog.prototype.getX = function() {
+	return this.x;
+};
+
+cog.prototype.getY = function() {
+	return this.y;
+};
+
+cog.prototype.getHeight = function() {
+	return this.height;
+};
+
+cog.prototype.getWidth = function() {
+	return this.width;
 };
 
 cog.prototype.getDirection = function() {
@@ -54,4 +89,8 @@ cog.prototype.getDirection = function() {
 
 cog.prototype.getSpeed = function() {
 	return this.speed;
+};
+
+cog.prototype.getColor = function() {
+	return this.color;
 };
---
+++ src/js/model.js
@@ -0,0 +1,7 @@
+/* This is where all data objects are stored, accessed, and modified. */
+var cogs = [];
+
+function addCog(shape, x, y, height, width, direction, speed, color) {
+  var cog = new Cog(shape, x, y, height, width, direction, speed, color);
+  cogs.push(cog);
+}
